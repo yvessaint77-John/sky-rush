6f17ebb324975333ef2b4e89cbb9dee565c35287
yvessaint77-John <yvessaint77@gmail.com>
2026-08-13 12:18:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,89 +1,15 @@
-const CACHE = 'sky-rush-v2';
-
-const FILES = [
-
-  './',
-
-  './index.html',
-
-  './manifest.json'
-
-];
-
-self.addEventListener('install', event => {
-
-  event.waitUntil(
-
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
-
-  );
-
+const CACHE = 'sky-rush-v3';
+const FILES = ['./SKY_RUSH_V3_index.html','./manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
-
 });
-
-self.addEventListener('activate', event => {
-
-  event.waitUntil(
-
-    caches.keys().then(keys =>
-
-      Promise.all(
-
-        keys
-
-          .filter(key => key !== CACHE)
-
-          .map(key => caches.delete(key))
-
-      )
-
-    )
-
-  );
-
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
-
 });
-
-self.addEventListener('fetch', event => {
-
-  if (event.request.mode === 'navigate') {
-
-    event.respondWith(
-
-      fetch(event.request)
-
-        .then(response => {
-
-          const copy = response.clone();
-
-          caches.open(CACHE).then(cache => {
-
-            cache.put('./index.html', copy);
-
-          });
-
-          return response;
-
-        })
-
-        .catch(() => caches.match('./index.html'))
-
-    );
-
-    return;
-
-  }
-
-  event.respondWith(
-
-    caches.match(event.request).then(
-
-      cached => cached || fetch(event.request)
-
-    )
-
-  );
-
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
