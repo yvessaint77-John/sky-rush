78f98740b5e118183c862622cf9419269d5a1d41
yvessaint77-John <yvessaint77@gmail.com>
2026-08-13 07:42:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,89 @@
-const CACHE='sky-rush-v1';
-const FILES=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+const CACHE = 'sky-rush-v2';
+
+const FILES = [
+
+  './',
+
+  './index.html',
+
+  './manifest.json'
+
+];
+
+self.addEventListener('install', event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+
+  );
+
   self.skipWaiting();
+
 });
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('activate', event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+
+          .filter(key => key !== CACHE)
+
+          .map(key => caches.delete(key))
+
+      )
+
+    )
+
+  );
+
+  self.clients.claim();
+
+});
+
+self.addEventListener('fetch', event => {
+
+  if (event.request.mode === 'navigate') {
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .then(response => {
+
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+
+            cache.put('./index.html', copy);
+
+          });
+
+          return response;
+
+        })
+
+        .catch(() => caches.match('./index.html'))
+
+    );
+
+    return;
+
+  }
+
+  event.respondWith(
+
+    caches.match(event.request).then(
+
+      cached => cached || fetch(event.request)
+
+    )
+
+  );
+
 });
